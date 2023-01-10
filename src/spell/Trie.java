77103fdaa8c8ec8d45b752d67ed678bd6c40559a
Josh Moody <joshmoody24@gmail.com>
2023-01-09 20:35:50 -0700

    Trie passes all test cases

--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -26,17 +26,26 @@ public class Trie implements ITrie {
                 nodeCount++;
             }
             iterations++;
-            if(iterations != word.length()){
-                currentNode = currentNode.getChildren()[index];
-            }
+            currentNode = currentNode.getChildren()[index];
         }
 
         currentNode.incrementValue();
-        wordCount++;
+        if(currentNode.getValue() == 1) wordCount++;
     }
 
     @Override
     public INode find(String word) {
+        INode currentNode = root;
+        for (char c : word.toLowerCase().toCharArray()) {
+            int index = c - 'a';
+            if(currentNode.getChildren()[index] != null){
+                currentNode = currentNode.getChildren()[index];
+            }
+            else{
+                return null;
+            }
+        }
+        if(currentNode.getValue() > 0) return currentNode;
         return null;
     }
 
@@ -52,7 +61,14 @@ public class Trie implements ITrie {
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        int hash = wordCount * nodeCount;
+        // mix in the indexes of the root node's non-null children
+        for(int i = 0; i < root.getChildren().length; i++){
+            if(root.getChildren()[i] != null){
+                hash *= i + 1;
+            }
+        }
+        return hash;
     }
 
     @Override
@@ -84,6 +100,39 @@ public class Trie implements ITrie {
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        if(obj == null) return false;
+        if(obj == this) return true;
+        if(obj.getClass() != this.getClass()) return false;
+        Trie other = (Trie)obj;
+        if(other.getNodeCount() != this.getNodeCount()) return false;
+        if(other.getWordCount() != this.getWordCount()) return false;
+
+        // traverse the trees to determine equality the hard way
+        return equals_Helper(other.root, root);
+    }
+
+    private boolean equals_Helper(INode n1, INode n2){
+        // compare n1 and n2 to see if they are the same
+        if(n1 == null && n2 == null) return true;
+        if(n1 == null && n2 != null) return false;
+        if(n1 != null && n2 == null) return false;
+        if(n1.getValue() != n2.getValue()) return false;
+
+        // not null children in same indices?
+        if(n1.getChildren().length != n2.getChildren().length) return false;
+        for(int i = 0; i < n1.getChildren().length; i++){
+            if(n1.getChildren()[i] == null && n2.getChildren()[i] != null) return false;
+            if(n2.getChildren()[i] == null && n1.getChildren()[i] != null) return false;
+        }
+        // recurse on children and compare child subtrees
+        // assume they are equal at first
+        boolean children_equal = true;
+        for(int i = 0; i < n1.getChildren().length; i++){
+            if(equals_Helper(n1.getChildren()[i], n2.getChildren()[i]) == false) {
+                children_equal = false;
+                break;
+            }
+        }
+        return children_equal;
     }
 }
